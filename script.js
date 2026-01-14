84e45aa0dc0f655ddea8a2a77c6d85504eee66cc
riveramichael716-del <riveramichael716@gmail.com>
2026-01-14 14:06:42 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function login() {
+  const user = document.getElementById("username").value;
+  const pass = document.getElementById("password").value;
+
+  if (user === "shane" && pass === "1234") {
+    window.location.href = "dashboard.html";
+  } else {
+    document.getElementById("error").innerText =
+      "Invalid username or password";
+  }
+}
